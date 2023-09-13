ce8cb432062e436d333df256ab4e3959c8947d33
Nikola <nikola.vlavcheski@students.finki.ukim.mk>
2023-09-13 13:31:11 +0200

    v1.3

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -91,7 +91,7 @@ function Nav() {
                         </Link>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                        <a className='flex justify-between items-center w-full text-gray-300' href="#"
+                        <a className='flex justify-between items-center w-full text-gray-300' href="/"
                             onClick={downloadCV} >
                             Resume <BsFillPersonLinesFill size={30} />
                         </a>
